3f1aff88c3255f38bac7859fa19f1fe80ea07df8
Florian <florian.engertsberger@gmail.com>
2021-11-12 10:57:52 +0100

    add selection of tags

--- src/data/datasets.js
+++ src/data/datasets.js
@@ -1,26 +1,21 @@
 export const datasets = [
     {
         "name": "Rubik's Cube: 1x2 Different Origins",
-        "id": 0,
+        "domain": "Rubik",
+        "id": "rubik_1_2_different_origins",
         "type": "Temporal Data",
         "description": "Rubik’s cube is a famous puzzle toy devised in 1974 by the Hungarian inventor and professor of architecture Ernő Rubik. The classic Rubik’s cube has six faces, with each face being made up by a 3 × 3 grid of colored facets. These facets are the faces of smaller cubes, the so-called cubies. The cube consists of 26 such cubies: 8 corner cubies with three facets each, 12 edge cubies with two facets each, and 6 center cubies with one facet each. The cube is considered solved when each of its faces shows only one color.",
         "tags": ["tabular", "numerical", "temporal", "games", "t-SNE", "single-run", "different-origins", "progressing", "intersections", "cluster", "cycle", "alternation"],
-        "imagePath": "rubik_0.png"
+        "imagePath": "rubik/rubik_1_2_different_origins.png"
     },
     {
         "name": "Rubik's Cube: 1x2 Different Origins",
-        "id": 1,
+        "domain": "Rubik",
+        "id": "sdf",
         "type": "Temporal Data",
         "description": "Rubik’s cube is a famous puzzle toy devised in 1974 by the Hungarian inventor and professor of architecture Ernő Rubik. The classic Rubik’s cube has six faces, with each face being made up by a 3 × 3 grid of colored facets. These facets are the faces of smaller cubes, the so-called cubies. The cube consists of 26 such cubies: 8 corner cubies with three facets each, 12 edge cubies with two facets each, and 6 center cubies with one facet each. The cube is considered solved when each of its faces shows only one color.",
-        "tags": ["Circles", "Bundles", "Points", "Speed"],
-        "imagePath": "rubik_0.png"
+        "tags": ["tabular", "numerical", "temporal", "games", "t-SNE", "single-run", "different-origins", "progressing", "intersections", "cluster", "cycle", "alternation"],
+        "imagePath": "rubik/rubik_1_2_different_origins.png"
     },
-    {
-        "name": "Rubik's Cube: 1x2 Different Origins",
-        "id": 2,
-        "type": "Temporal Data",
-        "description": "Rubik’s cube is a famous puzzle toy devised in 1974 by the Hungarian inventor and professor of architecture Ernő Rubik. The classic Rubik’s cube has six faces, with each face being made up by a 3 × 3 grid of colored facets. These facets are the faces of smaller cubes, the so-called cubies. The cube consists of 26 such cubies: 8 corner cubies with three facets each, 12 edge cubies with two facets each, and 6 center cubies with one facet each. The cube is considered solved when each of its faces shows only one color.",
-        "tags": ["Circles", "Bundles", "Points", "Speed"],
-        "imagePath": "rubik_0.png"
-    }
+
 ]
--- src/pages/Datasetspage.tsx
+++ src/pages/Datasetspage.tsx
@@ -1,11 +1,56 @@
+import React from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 import DatasetsCard from '../components/DatasetsCard';
 import { datasets } from '../data/datasets';
+import {TagsColorsIndex} from '../modules/Tags';
+
+const intersectArrays = (array1: any, array2: any) => {
+  return array1.filter((value: any) => array2.includes(value));
+}
+
+// Could be used if we want another filtering technique
+const checkSubsetArrays = (array1: any, array2: any) => {
+  return array1.every((val: any) => array2.includes(val));
+}
+
+const animatedComponents = makeAnimated();
+
+// prepare list of tags
+const tagList = Object.keys(TagsColorsIndex).filter((item) => {
+  return isNaN(Number(item));
+});
+
+// prepare options for selection
+const options = tagList.map((tag) => ({value: tag, label: tag}))
 
 function DatasetsPage() {
+  const [selectedOptions, setSelectedOptions] = React.useState();
+  const [selectedFilter, setselectedFilter] = React.useState([]);
+  console.log(selectedFilter)
+  
   return (
     <div>
+      <Select
+      className={"container w-50 mt-5"}
+      value={selectedOptions}
+      placeholder={"Select tags to filter datasets..."}
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      isMulti
+      options={options}
+      onChange={(selection: any) => {
+        setSelectedOptions(selection)
+        setselectedFilter(selection.map((option: any) => option.value))
+      }}
+    />
       <div className="mt-5 d-flex flex-row justify-content-around flex-wrap">
-        {datasets.map((dataset) => <DatasetsCard key={dataset.id} datasetInfo={dataset} />)}
+        {datasets.map((dataset) => {
+          if (checkSubsetArrays(selectedFilter, dataset.tags)) {
+            return <DatasetsCard key={dataset.id} datasetInfo={dataset} />
+          }
+        })}
+
       </div>
     </div>
   );
