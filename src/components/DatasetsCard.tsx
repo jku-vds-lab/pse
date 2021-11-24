0d9257f32cfb92a6c890a4c01aa0b9a67f248bb0
Florian <florian.engertsberger@gmail.com>
2021-11-24 17:13:42 +0100

    make dataset decrption toggleable

--- src/components/DatasetsCard.tsx
+++ src/components/DatasetsCard.tsx
@@ -1,5 +1,5 @@
-import '../styles/DatasetsCard.css';
 import { TagsColorEnum } from '../modules/Tags';
+import { useState } from 'react';
 
 interface IDatasetCardProps {
   datasetInfo: IDatasetInfo
@@ -7,8 +7,10 @@ interface IDatasetCardProps {
 
 interface IDatasetInfo {
   name: string;
+  id: string;
   type: string;
-  description: string;
+  primaryDescription: string;
+  secondaryDescription: string;
   tags: string[];
   imagePath: string;
   datasetLinks?: {};
@@ -16,24 +18,39 @@ interface IDatasetInfo {
 
 
 function DatasetsCard(props: IDatasetCardProps) {
+  const [collapseButton, setcollapseButton] = useState("Show more");
   const datasetImage = require(`../assets/datasetImages/${props.datasetInfo.imagePath}`).default;
 
   return (
-    <div className="card dataset-card mb-5">
-      <img src={datasetImage} alt={props.datasetInfo.name} className="card-img-top" />
-      <div className="card-body">
-        <h5 className="card-title">{props.datasetInfo.name}</h5>
-        <p className="card-text">{props.datasetInfo.description}</p>
-        <div className="mb-3">
-        {props.datasetInfo.datasetLinks &&
-          Object.entries(props.datasetInfo.datasetLinks).map(([key, value]) => <a key={key} href={String(value)} className="card-link">{key}</a>)
-        }
-        </div>
-        <div>
-          {props.datasetInfo.tags.map((tag: string, idx: number) => <span key={idx} className={`badge rounded-pill me-2 bg-${TagsColorEnum[tag as keyof typeof TagsColorEnum]} ${TagsColorEnum[tag as keyof typeof TagsColorEnum] === 'light' ? "text-dark" : ""}`} >{tag}</span>)}
+    <div className="col">
+      <div className="card dataset-card mb-5">
+        <img src={datasetImage} alt={props.datasetInfo.name} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{props.datasetInfo.name}</h5>
+          <p className="card-text mb-0">{props.datasetInfo.primaryDescription}</p>
+          <p className="card-text collapse mb-0" id={`description-${props.datasetInfo.id}`}>{props.datasetInfo.secondaryDescription}</p>
+          <button id={`collapseButton-${props.datasetInfo.id}`} className="btn btn-outline-secondary mt-3 mb-3" data-bs-toggle="collapse" data-bs-target={`#description-${props.datasetInfo.id}`} aria-expanded="false" aria-controls={`description-${props.datasetInfo.id}`}
+            onClick={() => {
+              if (document.getElementById(`collapseButton-${props.datasetInfo.id}`)?.classList.contains("collapsed")) {
+                setcollapseButton("Show more")
+              } else {
+                setcollapseButton("Show less")
+              }
+            }}>
+            {collapseButton}
+          </button>
+          <div className="mb-3">
+            {props.datasetInfo.datasetLinks &&
+              Object.entries(props.datasetInfo.datasetLinks).map(([key, value]) => <a key={key} href={String(value)} className="card-link">{key}</a>)
+            }
+          </div>
+          <div>
+            {props.datasetInfo.tags.map((tag: string, idx: number) => <span key={idx} className={`badge rounded-pill me-2 bg-${TagsColorEnum[tag as keyof typeof TagsColorEnum]} ${TagsColorEnum[tag as keyof typeof TagsColorEnum] === 'light' ? "text-dark" : ""}`} >{tag}</span>)}
+          </div>
         </div>
       </div>
     </div>
+
   );
 }
 
--- src/pages/Datasetspage.tsx
+++ src/pages/Datasetspage.tsx
@@ -3,7 +3,7 @@ import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import DatasetsCard from '../components/DatasetsCard';
 import { datasets } from '../data/datasets';
-import {TagsColorEnum} from '../modules/Tags';
+import { TagsColorEnum } from '../modules/Tags';
 
 // Could be used if we want another filtering technique
 const checkSubsetArrays = (array1: any, array2: any) => {
@@ -18,30 +18,29 @@ const tagList = Object.keys(TagsColorEnum).filter((item) => {
 });
 
 // prepare options for selection
-const options = tagList.map((tag) => ({value: tag, label: tag}))
+const options = tagList.map((tag) => ({ value: tag, label: tag }))
 
 function DatasetsPage() {
   const [selectedOptions, setSelectedOptions] = React.useState();
   const [selectedFilter, setselectedFilter] = React.useState([]);
-  
+
   return (
     <div>
       <Select
-      className={"container w-50 mt-5"}
-      value={selectedOptions}
-      placeholder={"Filter datasets by tags ..."}
-      closeMenuOnSelect={false}
-      components={animatedComponents}
-      isMulti
-      options={options}
-      onChange={(selection: any) => {
-        setSelectedOptions(selection)
-        setselectedFilter(selection.map((option: any) => option.value))
-      }}
-    />
-      <div className="mt-5 d-flex flex-row justify-content-around flex-wrap">
+        className={"container w-50 mt-5"}
+        value={selectedOptions}
+        placeholder={"Filter datasets by tags ..."}
+        closeMenuOnSelect={false}
+        components={animatedComponents}
+        isMulti
+        options={options}
+        onChange={(selection: any) => {
+          setSelectedOptions(selection)
+          setselectedFilter(selection.map((option: any) => option.value))
+        }}
+      />
+      <div className="mt-5 ms-3 me-3 row row-cols-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-3 g-4">
         {datasets.filter((dataset) => checkSubsetArrays(selectedFilter, dataset.tags)).map((dataset) => <DatasetsCard key={dataset.id} datasetInfo={dataset} />)}
-
       </div>
     </div>
   );
