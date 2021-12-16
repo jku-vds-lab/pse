c519c37b775f4d3c0910c570cec9871b0462875c
Florian <florian.engertsberger@gmail.com>
2021-12-16 13:48:36 +0100

    make taglend more compact

--- src/components/TagLegend.tsx
+++ src/components/TagLegend.tsx
@@ -10,8 +10,12 @@ const categoryList = Object.keys(categoryToColorEnum).filter((item) => {
 function Legend() {
     return (
         <div className='text-center'>
-          {categoryList.map((cat: string, idx: number) => <span key={idx} role="button" data-tip={categoryToDescriptionEnum[cat as keyof typeof categoryToDescriptionEnum]} className={`tag-legend-item badge rounded-pill me-4 mb-2 bg-${categoryToColorEnum[cat as keyof typeof categoryToColorEnum]} ${categoryToColorEnum[cat as keyof typeof categoryToColorEnum] === 'light' ? "text-dark" : ""}`} >{cat}</span>)}
-          <ReactTooltip place="top" type="dark" effect="solid" multiline={true}/>
+          {categoryList.map((cat: string, idx: number) => 
+          <span>
+            <span key={idx} role="button" data-for={cat} data-tip={categoryToDescriptionEnum[cat as keyof typeof categoryToDescriptionEnum]} className={`tag-legend-item badge rounded-pill me-3 bg-${categoryToColorEnum[cat as keyof typeof categoryToColorEnum]} ${categoryToColorEnum[cat as keyof typeof categoryToColorEnum] === 'light' ? "text-dark" : ""}`} >{cat}</span>
+            <ReactTooltip id={cat} type="dark" effect="solid" multiline={true} />
+          </span>
+          )}
         </div>
     )
 }
