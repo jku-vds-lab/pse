7dab287e38429ea37206362636cd3cfab67bb6b2
Florian <florian.engertsberger@gmail.com>
2022-01-16 13:07:49 +0100

    fix children unique key taglegend

--- src/components/TagLegend.tsx
+++ src/components/TagLegend.tsx
@@ -11,7 +11,7 @@ function Legend() {
     return (
         <div className='text-center'>
           {categoryList.map((cat: string, idx: number) => 
-          <span>
+          <span key={idx}>
             <span key={idx} role="button" data-for={cat} data-tip={categoryToDescriptionEnum[cat as keyof typeof categoryToDescriptionEnum]} className={`tag-legend-item badge rounded-pill me-3 bg-${categoryToColorEnum[cat as keyof typeof categoryToColorEnum]} ${categoryToColorEnum[cat as keyof typeof categoryToColorEnum] === 'light' ? "text-dark" : ""}`} >{cat}</span>
             <ReactTooltip id={cat} type="dark" effect="solid" multiline={true} />
           </span>
