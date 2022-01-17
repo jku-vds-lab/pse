830a3a52f395cd739b688a0e7430bb0fd51a9eb2
Florian <florian.engertsberger@gmail.com>
2022-01-17 14:27:39 +0100

    minor fix

--- src/components/TagLegend.tsx
+++ src/components/TagLegend.tsx
@@ -12,7 +12,7 @@ function Legend() {
         <div className='text-center'>
           {categoryList.map((cat: string, idx: number) => 
           <span key={idx}>
-            <span key={idx} role="button" data-for={cat} data-tip={categoryToDescriptionEnum[cat as keyof typeof categoryToDescriptionEnum]} className={`tag-legend-item badge rounded-pill me-3 bg-${categoryToColorEnum[cat as keyof typeof categoryToColorEnum]} ${categoryToColorEnum[cat as keyof typeof categoryToColorEnum] === 'light' ? "text-dark" : ""}`} >{cat}</span>
+            <span role="button" data-for={cat} data-tip={categoryToDescriptionEnum[cat as keyof typeof categoryToDescriptionEnum]} className={`tag-legend-item badge rounded-pill me-3 bg-${categoryToColorEnum[cat as keyof typeof categoryToColorEnum]} ${categoryToColorEnum[cat as keyof typeof categoryToColorEnum] === 'light' ? "text-dark" : ""}`} >{cat}</span>
             <ReactTooltip id={cat} type="dark" effect="solid" multiline={true} />
           </span>
           )}
