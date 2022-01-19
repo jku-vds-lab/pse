59d7c9d5dda5038d338995fe6c4bbd673df1b388
Florian <florian.engertsberger@gmail.com>
2022-01-19 17:06:51 +0100

    add publication images

--- src/components/PublicationsCard.tsx
+++ src/components/PublicationsCard.tsx
@@ -1,19 +1,27 @@
-
 interface IPublicationsCardProps {
     title: string;
     subTitle?: string;
     description: string;
     link: string;
+    imagePath: string;
 }
 
 function PublicationsCard(props: IPublicationsCardProps) {
+    const datasetImage = require(`../assets/publicationImages/${props.imagePath}`);
     return (
-        <div className="card flex-fill mb-5">
-            <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.subTitle}</h6>
-                <p className="card-text">{props.description}</p>
-                <p>You can find this paper <a href={props.link} className="card-link">here</a>.</p>
+        <div className="card mb-5">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={datasetImage} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{props.subTitle}</h6>
+                        <p className="card-text">{props.description}</p>
+                        <p>You can find this paper <a href={props.link} className="card-link">here</a>.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
