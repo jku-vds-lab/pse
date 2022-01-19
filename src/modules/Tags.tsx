014a2e9b51afbad96a2bb1a0e1766deffb31737a
Florian <florian.engertsberger@gmail.com>
2022-01-19 14:17:49 +0100

    change colors to vds scheme

--- src/modules/Tags.tsx
+++ src/modules/Tags.tsx
@@ -23,10 +23,10 @@ export enum categoryToDescriptionEnum {
 
 export enum bsColorToHex {
   "primary" = "#007dad",
-  "secondary" = "#6c757d",
-  "success" = "#198754",
-  "danger" = "#dc3545",
-  "warning" = "#ffc107",
+  "secondary" = "#b2d349",
+  "success" = "#ae619d",
+  "danger" = "#f15a4e",
+  "warning" = "#fdb812",
   "info" = "#0AACCC",
   "light" = "#B1B2B3",
   "dark" = "#212529",
